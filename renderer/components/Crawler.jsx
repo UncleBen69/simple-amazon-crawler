ee2c4a34d100d79ab1f1a0d4206f3ee92b0cda9c
Vadim Alexeev <valexeev30@gmail.com>
2020-08-23 18:46:58 +0100

    Remove Scroll bar

--- renderer/components/Crawler.jsx
+++ renderer/components/Crawler.jsx
@@ -430,7 +430,7 @@ class Crawler extends React.Component{
 								min-width: 25vw;
 								background-color: #202124;
 								box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-								overflow-y: scroll;
+								overflow-y: hidden;
 								text-align: center;
 							}
 						`}</style>
